83716d84cde0efc6a6530adcd9b0219bdbb20e34
tobiasKaminsky <tobias@kaminsky.me>
2025-08-20 09:06:24 +0200

    update

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -26,9 +26,9 @@ class Capabilities implements ICapability
 						"endpoints" => [
 							["name" => "List all UI elements", "url" => "/ocs/v2.php/apps/declarativetest/all"],
 							["name" => "First version", "url" => "/ocs/v2.php/apps/declarativetest/version1"],
-							["name" => "Convert to PDF", "url" => "/ocs/v2.php/apps/declarativetest/version1", "filter" => "image/"],
+							["name" => "Convert to PDF", "url" => "/ocs/v2.php/apps/declarativetest/convertFile/123123", "filter" => "image/"],
 							["name" => "Create transcript", "url" => "/ocs/v2.php/apps/declarativetest/version1", "filter" => "audio/", "android_icon" => "file_sound"],
-							["name" => "Create zip file", "url" => "/ocs/v2.php/apps/declarativetest/version1", "android_icon" => "file_zip", "ios_icon" => "zip", "desktop_icon" => "zip"],
+							["name" => "Create zip file", "url" => "/ocs/v2.php/apps/file_zip/zip/1231123", "android_icon" => "file_zip", "ios_icon" => "zip", "desktop_icon" => "zip"],
 							["name" => "Unzip", "url" => "/ocs/v2.php/apps/declarativetest/version1", "filter" => "application/zip", "android_icon" => "file_zip"],
 						]
 					],

d63a2d874c73b23a2c709876047b70f19ad1036c
tobiasKaminsky <tobias@kaminsky.me>
2025-09-08 08:45:42 +0200

    update api

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace OCA\DeclarativeTest;
 
+use OCA\DeclarativeTest\AppInfo\Application;
 use OCP\Capabilities\ICapability;
 
 class Capabilities implements ICapability
@@ -20,25 +21,19 @@ class Capabilities implements ICapability
 	{
 		return [
 			'declarativeui' => [
-				"hooks" => [
-					[
-						"type" => "context-menu",
-						"endpoints" => [
-							["name" => "List all UI elements", "url" => "/ocs/v2.php/apps/declarativetest/all"],
-							["name" => "First version", "url" => "/ocs/v2.php/apps/declarativetest/version1"],
-							["name" => "Convert to PDF", "url" => "/ocs/v2.php/apps/declarativetest/convertFile/123123", "filter" => "image/"],
-							["name" => "Create transcript", "url" => "/ocs/v2.php/apps/declarativetest/version1", "filter" => "audio/", "android_icon" => "file_sound"],
-							["name" => "Create zip file", "url" => "/ocs/v2.php/apps/file_zip/zip/1231123", "android_icon" => "file_zip", "ios_icon" => "zip", "desktop_icon" => "zip"],
-							["name" => "Unzip", "url" => "/ocs/v2.php/apps/declarativetest/version1", "filter" => "application/zip", "android_icon" => "file_zip"],
-						]
+				Application::APP_ID => [
+					'context-menu' => [
+						["name" => "List all UI elements", "url" => "/ocs/v2.php/apps/declarativetest/all"],
+						["name" => "First version", "url" => "/ocs/v2.php/apps/declarativetest/version1"],
+						["name" => "Convert to PDF", "url" => "/ocs/v2.php/apps/declarativetest/convertFile/123123", "filter" => "image/"],
+						["name" => "Create transcript", "url" => "/ocs/v2.php/apps/declarativetest/version1", "filter" => "audio/", "android_icon" => "file_sound"],
+						["name" => "Create zip file", "url" => "/ocs/v2.php/apps/file_zip/zip/1231123", "android_icon" => "file_zip", "ios_icon" => "zip", "desktop_icon" => "zip"],
+						["name" => "Unzip", "url" => "/ocs/v2.php/apps/declarativetest/version1", "filter" => "application/zip", "android_icon" => "file_zip"],
 					],
-					[
-						"type" => "create-new",
-						"endpoints" => [
-							["name" => "Deck board", "url" => "/ocs/v2.php/apps/declarativetest/newDeckBoard"],
-							["name" => "New Contact", "url" => "/ocs/v2.php/apps/declarativetest/newContact", "android_icon" => "file_vcard"],
-						]
-					],
+					'create-new' => [
+						["name" => "Deck board", "url" => "/ocs/v2.php/apps/declarativetest/newDeckBoard"],
+						["name" => "New Contact", "url" => "/ocs/v2.php/apps/declarativetest/newContact", "android_icon" => "file_vcard"],
+					]
 				]
 			]
 		];
